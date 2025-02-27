05aa7d83e050db2a4b7d814d838a1e748e5fea88
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2025-02-26 23:20:29 -0300

    f

---
+++ Semana 0/monster hunter.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll dp(int v, int round, vector<vector<int> >& edges, vector<ll>& a, vector<vector<ll> >& memo, int dad) {
+
+    if (memo[v][round] != -1)
+        return memo[v][round];
+    
+    ll res = 0;
+    int r1 = (round + 1) % 3;
+    int r2 = (round + 2) % 3;
+    
+    for (auto& vert : edges[v]) {
+        if (vert != dad) {
+            res += min(dp(vert, r1, edges, a, memo, v),
+                       dp(vert, r2, edges, a, memo, v));
+        }
+    }
+
+    res += a[v] * (round+1);
+
+    return memo[v][round] = res;
+}
+
+int main() {
+    int t; cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+        vector<ll> a(n+1);
+        vector<vector<int> > edges(n+1);
+
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+        }
+
+        for (int i = 1; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            edges[x].push_back(y);
+            edges[y].push_back(x);
+        }
+
+        if      (n == 1) cout << a[1] << endl;
+        else if (n == 2) cout << max(a[1], a[2]) + 2 * min(a[1], a[2]) << endl;
+
+        else {
+            int root = 0;
+            for (int i = 1; i <= n; i++) {
+                if (edges[i].size() > 1) {
+                    root = i;
+                    break;
+                }
+            }
+
+            vector<vector<ll> > memo(n+1, vector<ll>(3, -1));
+
+            ll ans = dp(root, 0, edges, a, memo, 0);
+            ans = min(ans, dp(root, 1, edges, a, memo, 0));
+            ans = min(ans, dp(root, 2, edges, a, memo, 0));
+
+            cout << ans << endl;
+        }
+    }
+
+    return 0;
+}
