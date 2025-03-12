15bf1245a2c83f0c8f789d6c3997edc822f1c729
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2025-03-11 23:46:21 -0300

    Semana 2 feita

--- Semana 2/move back.cpp
+++ Semana 2/move back.cpp
@@ -7,6 +7,43 @@ int main() {
 
     while (t--) {
         int n; cin >> n;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<int> v(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+            pq.push(make_pair(v[i], i));
+        }
+
+        vector<int> first;
+        priority_queue<int, vector<int>, greater<int>> last;
+
+        for (int i = 0; i < n; i++) {
+            if (!pq.empty() && v[i] == pq.top().first) {
+                pq.pop();
+                
+                if (!last.empty() && v[i] > last.top())
+                    last.push(v[i] + 1);
+                else
+                    first.push_back(v[i]);
+
+                while (!pq.empty() && pq.top().second < i) {
+                    pq.pop();
+                }
+            }
+            else {
+                last.push(v[i] + 1);
+            }
+        }
+
+        for (int i = 0; i < first.size(); i++) {
+            cout << first[i] << " ";
+        }
+        while (!last.empty()) {
+            cout << last.top() << " ";
+            last.pop();
+        }
+        cout << endl;
     }
 
     return 0;
