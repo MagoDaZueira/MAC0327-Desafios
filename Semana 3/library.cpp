8c8b7752c656948c13b614ddb019233c0c94ade9
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2025-03-21 01:00:48 -0300

    Semana 3 final

--- Semana 3/library.cpp
+++ Semana 3/library.cpp
@@ -2,14 +2,16 @@
 #define ll long long int
 using namespace std;
 
-ll maior_bit(ll num) {
+ll total_xor;
+
+ll casas_binarias(ll num) {
     if (num == 0) return 0;
-    ll msb = 0;
+    ll conta = 0;
     while (num > 1) {
         num >>= 1;
-        msb++;
+        conta++;
     }
-    return (1LL << msb);
+    return conta;
 }
 
 ll xor_query(ll l, ll r) {
@@ -20,73 +22,81 @@ ll xor_query(ll l, ll r) {
     return out;
 }
 
-void bin_search(ll lo, ll hi, vector<ll>& ans) {
+void bin_search(ll lo, ll hi, vector<ll>& ans, ll xor_intervalo) {
     ll mid;
-    ll last_xor_r = -1;
-    bool mesmo_bit = false;
+    ll potencia = casas_binarias(hi);
     while (ans.size() < 2) {
         if (hi <= lo) {
-            // cout << "Adding to vec: " << lo << " " << hi;
             ans.push_back(lo);
             return;
         }
 
-        ll msb_r = maior_bit(hi);
+        if (hi - lo == 1) mid = hi;
+        else              mid = (lo + hi) / 2;
 
-        if (hi - lo == 1) {
-            mid = hi;
-        }
-        else if (msb_r <= lo) {
-            mid = (lo + hi) / 2;
-            mesmo_bit = true;
-        }
-        else {
-            mid = msb_r;
-        }
-
-        // cout << "Quering right: ";
         ll xor_r = xor_query(mid, hi);
-        if (last_xor_r == -1) last_xor_r = xor_r;
+
         if (xor_r == 0) {
             hi = mid - 1;
         }
+        else if (xor_r != xor_intervalo) {
+            ll casas_xor_r = casas_binarias(xor_r);
 
+            if (casas_xor_r < potencia) {
+                ll xor_l = xor_query(lo, mid-1);
+                ans.push_back(xor_l);
+                xor_intervalo ^= xor_l;
+                lo = mid;
+            }
+            else {
+                hi = mid - 1;
+                xor_intervalo ^= xor_r;
+                ans.push_back(xor_r);
+            }
+        }
         else {
-            if (!mesmo_bit) {
-                // cout << "Quering left: ";
-                ll xor_l = xor_query(lo, mid-1);
-                if (xor_l == 0) {
-                    lo = mid;
-                }
-                else {
-                    bin_search(mid, hi, ans);
-                    bin_search(lo, mid-1, ans);
-                }
-            }
-            else if (last_xor_r != xor_r) {
-                // cout << "Quering left: ";
-                ll xor_l = xor_query(lo, mid-1);
-                if (xor_l == 0) {
-                    lo = mid;
-                }
-                else {
-                    bin_search(mid, hi, ans);
-                    bin_search(lo, mid-1, ans);
-                }
-            }
             lo = mid;
         }
-
-        last_xor_r = xor_r;
     }
 }
 
 void solve(ll n) {
-    ll total_xor = xor_query(1, n);
+    total_xor = xor_query(1, n);
     vector<ll> ans;
 
-    ll lo = 1; ll hi = n;
-    bin_search(lo, hi, ans);
+    vector<pair<ll, ll>> candidates;
+    for (ll i = 1; i < n; i <<= 1) {
+        ll out = xor_query(i, min((i << 1) - 1, n));
+        if (out != 0) {
+            candidates.push_back({i, out});
+        }
+    }
+
+    if (candidates.size() == 3) {
+        ans.push_back(candidates[0].second);
+        ans.push_back(candidates[1].second);
+    }
+    
+    else if (candidates.size() == 2) {
+        ll i, out;
+        if (casas_binarias(candidates[0].second) != casas_binarias(candidates[0].first)) {
+            i = candidates[0].first;
+            out = candidates[0].second;
+            ans.push_back(candidates[1].second);
+        }
+        else {
+            i = candidates[1].first;
+            out = candidates[1].second;
+            ans.push_back(candidates[0].second);
+        }
+        bin_search(i, min((i << 1) - 1, n), ans, out);
+    }
+
+    else {
+        ll i = candidates[0].first;
+        ll out = candidates[0].second;
+        bin_search(i, min((i << 1) - 1, n), ans, out);
+    }
 
     ll last_num = total_xor ^ ans[0] ^ ans[1];
     ans.push_back(last_num);
