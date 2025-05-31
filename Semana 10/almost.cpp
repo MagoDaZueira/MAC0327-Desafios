eff0faa182adc34c592b1ce3182e8bd0ce4a56f4
MagoDaZueira <otaviogarciacapobianco@gmail.com>
2025-05-31 20:02:42 -0300

    Acho que não realizarei o último slk

--- Semana 10/almost.cpp
+++ Semana 10/almost.cpp
@@ -3,31 +3,21 @@ using namespace std;
 #define ll long long int
 
 const int MAXN = 100005;
-vector<int> parent(MAXN);
+vector<int> parent(2 * MAXN);
 vector<int> depth(MAXN, 0);
 
-vector<set<int>> children(MAXN);
+vector<int> actual(MAXN);
 vector<int> amount(MAXN);
 vector<ll> sum(MAXN);
 
 int find(int a) {
     if (parent[a] == a) return a;
-    int root = find(parent[a]);
-    // if (parent[a] != root) {
-        children[parent[a]].erase(a);
-        children[root].insert(a);
-    // }
-    return parent[a] = root;
+    return parent[a] = find(parent[a]);
 }
-// int find(int a) {
-//     if (parent[a] == a) return a;
-//     return find(parent[a]);
-//     // return parent[a] = find(parent[a]);
-// }
 
 void unite(int a, int b) {
-    int root_a = find(a);
-    int root_b = find(b);
+    int root_a = find(actual[a]);
+    int root_b = find(actual[b]);
 
     if (root_a == root_b) return;
 
@@ -35,49 +25,34 @@ void unite(int a, int b) {
         parent[root_a] = root_b;
         amount[root_b] += amount[root_a];
         sum[root_b] += sum[root_a];
-        children[root_b].insert(root_a);
     }
     else if (depth[root_a] > depth[root_b]) {
         parent[root_b] = root_a;
         amount[root_a] += amount[root_b];
         sum[root_a] += sum[root_b];
-        children[root_a].insert(root_b);
     }
     else {
         parent[root_b] = root_a;
         depth[root_a]++;
         amount[root_a] += amount[root_b];
         sum[root_a] += sum[root_b];
-        children[root_a].insert(root_b);
     }
 }
 
+int current_actual;
 void transfer(int a, int b) {
-    int root_a = find(a);
-    int root_b = find(b);
+    int root_a = find(actual[a]);
+    int root_b = find(actual[b]);
     if (root_a == root_b) return;
 
-    if (root_a == a && !children[a].empty()) {
-        root_a = *children[a].begin();
-        amount[root_a] = amount[a];
-        sum[root_a] = sum[a];
-        parent[root_a] = root_a;
-    }
-    for (auto child : children[a]) {
-        if (child != root_a) {
-            children[root_a].insert(child);
-            parent[child] = root_a;
-        }
-    }
-    children[a].clear();
-    children[root_b].insert(a);
+    int new_actual = current_actual++;
 
     amount[root_a]--;
     amount[root_b]++;
     sum[root_a] -= a;
     sum[root_b] += a;
-    children[parent[a]].erase(a);
-    parent[a] = root_b;
+    parent[new_actual] = root_b;
+    actual[a] = new_actual;
 }
 
 void solve(int n, int m) {
@@ -86,8 +61,9 @@ void solve(int n, int m) {
         parent[i] = i;
         amount[i] = 1;
         sum[i] = i;
-        children[i].clear();
+        actual[i] = i;
     }
+    current_actual = n + 1;
 
     while (m--) {
         int op; cin >> op;
@@ -100,7 +76,7 @@ void solve(int n, int m) {
             transfer(p, q);
         }
         else {
-            int root = find(p);
+            int root = find(actual[p]);
             cout << amount[root] << " " << sum[root] << endl;
         }
     }
